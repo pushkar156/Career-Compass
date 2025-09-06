45bfb6ace6f6e6a0eaf62c9379c66503eb3e6f0b
Mitali Agrawal <agrawalmitali728@gmail.com>
2025-09-06 09:26:31 +0000

    remove this dynamic moving down option from everywhere

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -73,22 +73,6 @@ export default {
         sm: 'calc(var(--radius) - 4px)',
       },
       keyframes: {
-        'accordion-down': {
-          from: {
-            height: '0',
-          },
-          to: {
-            height: 'var(--radix-accordion-content-height)',
-          },
-        },
-        'accordion-up': {
-          from: {
-            height: 'var(--radix-accordion-content-height)',
-          },
-          to: {
-            height: '0',
-          },
-        },
         'bg-pan': {
           '0%': { backgroundPosition: '0% 50%' },
           '50%': { backgroundPosition: '100% 50%' },
@@ -96,8 +80,6 @@ export default {
         },
       },
       animation: {
-        'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out',
         'bg-pan': 'bg-pan 12s linear infinite',
       },
     },
